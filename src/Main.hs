9b4ecd250684c1c7f029d88d299f51fd7e43a91b
Magnus Therning <magnus@therning.org>
2018-09-07 22:48:37 +0200

    Do the JSON logging ourselves instead
    Signed-off-by: Magnus Therning <magnus@therning.org>

--- src/Main.hs
+++ src/Main.hs
@@ -1,21 +1,29 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 module Main where
 
 import Control.Monad.IO.Class (liftIO)
-import Data.Aeson (decode, object, toJSON, (.=))
-import Data.Default (def)
-import Network.Wai.Middleware.RequestLogger
-import Network.Wai.Middleware.RequestLogger.JSON (formatAsJSON)
-import Web.Scotty
+import Control.Monad.Reader
+import Data.Aeson (decode, encode, object, toJSON, (.=))
+import Data.Aeson.Types
+import Data.ByteString.Char8 as BS8 (unpack)
+import Data.ByteString as BS (ByteString)
+import Data.CaseInsensitive (CI, original)
+import Data.Text.Lazy
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Handler.Warp (Port)
+import System.Log.FastLogger
+import Web.Scotty.Trans
 
 import SystemInfo
 
 main :: IO ()
 main = do
   putStrLn "Starting..."
-  logStdoutJSON <- mkRequestLogger (def { outputFormat = CustomOutputFormatWithDetails formatAsJSON})
-  scotty 3000 $ do
-    middleware logStdoutJSON
+  logger <- newStdoutLoggerSet defaultBufSize
+  chell logger 3000 $ do
+    middleware $ requestLogger logger
+    middleware $ responseLogger logger
     get "/status" $ do
       json $ toJSON mySI
     post "/status" $ do
@@ -27,3 +35,43 @@ main = do
           sis <- liftIO $ mapM getSystemInfo urls'
           json $ toJSON $ (Right mySI) : sis
     notFound $ json $ object [ "error" .= ("not found" :: String) ]
+
+type ChellM c = ScottyT Text (ReaderT c IO)
+type ChellActionM c = ActionT Text (ReaderT c IO)
+
+chell :: c -> Port -> ChellM c () -> IO ()
+chell cfg port a = scottyT port (flip runReaderT cfg) a
+
+requestLogger :: LoggerSet -> Middleware
+requestLogger l app req sendResponse = do
+  pushLogStrLn l $ reqToLogStr req
+  app req $ \ res -> sendResponse res
+
+responseLogger :: LoggerSet -> Middleware
+responseLogger l app req sendResponse =
+  app req $ \ res -> do
+    pushLogStrLn l $ resToLogStr res
+    sendResponse res
+
+reqToLogStr :: Request -> LogStr
+reqToLogStr r = toLogStr $ encode $ object [ "lvl" .= ("debug (replace with type!!!)" :: String)
+                                           , "type" .= ("response" :: String)
+                                           , "@timestamp" .= ("TBD" :: String)
+                                           , "headers" .= (toJSON $ requestHeaders r)]
+
+resToLogStr :: Response -> LogStr
+resToLogStr r = toLogStr $ encode $ object [ "lvl" .= ("debug (replace with type!!!)" :: String)
+                                           , "type" .= ("response" :: String)
+                                           , "@timestamp" .= ("TBD" :: String)
+                                           , "status" .= (toJSON $ responseStatus r)
+                                           , "headers" .= (toJSON $ responseHeaders r)]
+
+instance ToJSON Status where
+  toJSON Status{..} = object [ "code" .= (toJSON statusCode)
+                             , "message" .= BS8.unpack statusMessage]
+
+instance ToJSON ByteString where
+  toJSON = toJSON . BS8.unpack
+
+instance ToJSON c => ToJSON (CI c) where
+  toJSON = toJSON . original
