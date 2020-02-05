cacefac62812e06c50688b8664c96df69dc62380
Magnus Therning <magnus@therning.org>
2020-02-05 14:08:25 +0100

    Pin ghcide-for-nix to version with ghcide 0.1.0

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with (import (builtins.fetchGit {
   name = "ghcide-for-nix";
   url = https://github.com/magthe/ghcide-for-nix;
-  rev = "be2220f1269cfdb5d5eb877ca5424b6e9c8e5d5b";
+  rev = "f48111e41c7a2b1b05bf46ddc4b46928d2a3e8d7";
 }) );
 
 let
